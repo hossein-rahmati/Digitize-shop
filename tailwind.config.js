1c051cc4f00d66b26df3869e16f0a6be914f4819
hossein rahmati <rmthossein@gmail.com>
2023-08-12 22:19:37 +0330

    update sources

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./docs/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
